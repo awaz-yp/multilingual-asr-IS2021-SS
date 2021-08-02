dac2d940d63126b265346134d01a861a1b487395
meet.soni <meet.soni@tcs.com>
2021-08-02 17:27:46 +0530

    README

--- README.md
+++ README.md
@@ -4,4 +4,15 @@
 * s5 contains recipe for kaldi system
 
 ## ESPNet System Training
+* Install ESPNet (commit 53f6aa11d8702c76fc3220ca94ea279a11dbcf19) and move asr1 as `espnet/egs/MUCS/asr1`
+* cd to `espnet/egs/MUCS/asr1`
+* Checkout `run.sh` and run it after changing path to dataset
 
+## Kaldi System Training
+* Install kaldi and move s5 as `kaldi/egs/MUCS/s5`
+* cd to `kaldi/egs/MUCS/s5`
+* Checkout `run.sh` and run it after changing path to dataset
+
+## Evaluate pre-trained system
+* The pre-trained model and instructions to run can be found [here](https://drive.google.com/file/d/1ZSn1nMS7CVBFHweiu9UUtVUJaMklkAyo/view?usp=sharing)
+* System description can be found [here](https://drive.google.com/file/d/1n1fT2kgvBw0Veeni3SlGS90Rglm5zmZV/view?usp=sharing)
--- asr1/run.sh
+++ asr1/run.sh
@@ -73,8 +73,8 @@ train_set_actual=train_all
 train_sp=train_all_sp
 train_dev=dev
 train_test=dev
-#recog_set="Gujarati_test Hindi_test Odia_test Tamil_test Telugu_test Marathi_test"
-recog_set="Tamil_test Telugu_test Marathi_test"
+recog_set="Gujarati_test Hindi_test Odia_test Tamil_test Telugu_test Marathi_test"
+#recog_set="Tamil_test Telugu_test Marathi_test"
 
 
 if [ ${stage} -le -1 ] && [ ${stop_stage} -ge -1 ]; then
@@ -104,56 +104,56 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     fbankdir=fbank
     # Generate the fbank features; by default 80-dimensional fbanks with pitch on each frame
     
-    #for x in Hindi_train Hindi_test Gujarati_train Gujarati_test Marathi_train Marathi_test Tamil_train Tamil_test Telugu_train Telugu_test Odia_train Odia_test; do
+    for x in Hindi_train Hindi_test Gujarati_train Gujarati_test Marathi_train Marathi_test Tamil_train Tamil_test Telugu_train Telugu_test Odia_train Odia_test; do
 
-    #for x in Telugu_train Telugu_test; do
-    #    steps/make_fbank.sh --cmd "$train_cmd" --nj ${nj} --write_utt2num_frames true \
-    #        data/${x} exp/make_fbank/${x} ${fbankdir}
-    #    utils/fix_data_dir.sh data/${x}
-    #done
 
-    #utils/combine_data.sh --extra_files utt2num_frames --extra_files feats.scp data/${train_set}_orig data/Hindi_train data/Gujarati_train data/Marathi_train data/Telugu_train data/Tamil_train data/Odia_train
-    #utils/combine_data.sh --extra_files utt2num_frames --extra_files feats.scp  data/${train_dev}_orig data/Hindi_test data/Gujarati_test data/Marathi_test data/Telugu_test data/Tamil_test data/Odia_test
+       steps/make_fbank.sh --cmd "$train_cmd" --nj ${nj} --write_utt2num_frames true \
+           data/${x} exp/make_fbank/${x} ${fbankdir}
+       utils/fix_data_dir.sh data/${x}
+    done
+
+    utils/combine_data.sh --extra_files utt2num_frames --extra_files feats.scp data/${train_set}_orig data/Hindi_train data/Gujarati_train data/Marathi_train data/Telugu_train data/Tamil_train data/Odia_train
+    utils/combine_data.sh --extra_files utt2num_frames --extra_files feats.scp  data/${train_dev}_orig data/Hindi_test data/Gujarati_test data/Marathi_test data/Telugu_test data/Tamil_test data/Odia_test
 
     # remove utt having more than 3000 frames
     # remove utt having more than 400 characters
-    #remove_longshortdata.sh --maxframes 3000 --maxchars 400 data/${train_set}_orig data/${train_set}
-    #remove_longshortdata.sh --maxframes 3000 --maxchars 400 data/${train_dev}_orig data/${train_dev}
+    remove_longshortdata.sh --maxframes 3000 --maxchars 400 data/${train_set}_orig data/${train_set}
+    remove_longshortdata.sh --maxframes 3000 --maxchars 400 data/${train_dev}_orig data/${train_dev}
     
-    #utils/combine_data.sh --extra_files utt2num_frames data/${train_dev}_org data/dev_clean data/dev_other
+    utils/combine_data.sh --extra_files utt2num_frames data/${train_dev}_org data/dev_clean data/dev_other
     
-    #utils/perturb_data_dir_speed.sh 0.9  data/train_all  data/temp1
-    #utils/perturb_data_dir_speed.sh 1.0  data/train_all  data/temp2
-    #utils/perturb_data_dir_speed.sh 1.1  data/train_all  data/temp3
+    utils/perturb_data_dir_speed.sh 0.9  data/train_all  data/temp1
+    utils/perturb_data_dir_speed.sh 1.0  data/train_all  data/temp2
+    utils/perturb_data_dir_speed.sh 1.1  data/train_all  data/temp3
 
-    #utils/perturb_data_dir_speed.sh 0.9  data/dev  data/temp1
-    #utils/perturb_data_dir_speed.sh 1.0  data/dev  data/temp2
-    #utils/perturb_data_dir_speed.sh 1.1  data/dev  data/temp3
+    utils/perturb_data_dir_speed.sh 0.9  data/dev  data/temp1
+    utils/perturb_data_dir_speed.sh 1.0  data/dev  data/temp2
+    utils/perturb_data_dir_speed.sh 1.1  data/dev  data/temp3
 
-    #utils/combine_data.sh --extra-files utt2uniq data/train_dev_all_sp data/train_all_sp data/temp1 data/temp2 data/temp3
+    utils/combine_data.sh --extra-files utt2uniq data/train_dev_all_sp data/train_all_sp data/temp1 data/temp2 data/temp3
     
-    #utils/data/perturb_data_dir_volume.sh ${train_sp}
+    utils/data/perturb_data_dir_volume.sh ${train_sp}
 
-    #steps/make_fbank.sh --cmd "$train_cmd" --nj $nj  --write_utt2num_frames true \
-    #    data/train_dev_all_sp  exp/make_fbank/train_dev_all_sp  ${fbankdir}
+    steps/make_fbank.sh --cmd "$train_cmd" --nj $nj  --write_utt2num_frames true \
+        data/train_dev_all_sp  exp/make_fbank/train_dev_all_sp  ${fbankdir}
     
-    #utils/fix_data_dir.sh data/${train_sp}
+    utils/fix_data_dir.sh data/${train_sp}
     # compute global CMVN
     
-   # compute-cmvn-stats scp:data/${train_sp}/feats.scp data/${train_sp}/cmvn.ark
+   compute-cmvn-stats scp:data/${train_sp}/feats.scp data/${train_sp}/cmvn.ark
 
     # dump features for training
     dump.sh --cmd "$train_cmd" --nj ${nj} --do_delta ${do_delta} \
         data/train_dev_all_sp/feats.scp data/train_all/cmvn.ark exp/dump_feats/train ${feat_sp_dir}
-    #dump.sh --cmd "$train_cmd" --nj ${nj} --do_delta ${do_delta} \
-    #    data/${train_dev}/feats.scp data/${train_sp}/cmvn.ark exp/dump_feats/dev ${feat_dt_dir}
+    dump.sh --cmd "$train_cmd" --nj ${nj} --do_delta ${do_delta} \
+        data/${train_dev}/feats.scp data/${train_sp}/cmvn.ark exp/dump_feats/dev ${feat_dt_dir}
     
-    #for rtask in ${recog_set}; do
-    #    feat_recog_dir=${dumpdir}/${rtask}/delta${do_delta}; mkdir -p ${feat_recog_dir}
-    #    dump.sh --cmd "$train_cmd" --nj ${nj} --do_delta ${do_delta} \
-    #        data/${rtask}/feats.scp data/${train_sp}/cmvn.ark exp/dump_feats/recog/${rtask} \
-    #        ${feat_recog_dir}
-    #done
+    for rtask in ${recog_set}; do
+        feat_recog_dir=${dumpdir}/${rtask}/delta${do_delta}; mkdir -p ${feat_recog_dir}
+        dump.sh --cmd "$train_cmd" --nj ${nj} --do_delta ${do_delta} \
+            data/${rtask}/feats.scp data/${train_sp}/cmvn.ark exp/dump_feats/recog/${rtask} \
+            ${feat_recog_dir}
+    done
 fi
 
 dict=data/lang_char/${train_set}_${bpemode}${nbpe}_units.txt
@@ -164,23 +164,23 @@ if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
     echo "stage 2: Dictionary and Json Data Preparation"
     mkdir -p data/lang_char/
     
-    #echo "<unk> 1" > ${dict} # <unk> must be 1, 0 will be used for "blank" in CTC
-    #cut -f 2- -d" " data/${train_set}/text > data/lang_char/input.txt
-    #spm_train --input=data/lang_char/input.txt --vocab_size=${nbpe} --model_type=${bpemode} --model_prefix=${bpemodel} --input_sentence_size=100000000
-    #spm_encode --model=${bpemodel}.model --output_format=piece < data/lang_char/input.txt | tr ' ' '\n' | sort | uniq | awk '{print $0 " " NR+1}' >> ${dict}
-    #wc -l ${dict}
+    echo "<unk> 1" > ${dict} # <unk> must be 1, 0 will be used for "blank" in CTC
+    cut -f 2- -d" " data/${train_set}/text > data/lang_char/input.txt
+    spm_train --input=data/lang_char/input.txt --vocab_size=${nbpe} --model_type=${bpemode} --model_prefix=${bpemodel} --input_sentence_size=100000000
+    spm_encode --model=${bpemodel}.model --output_format=piece < data/lang_char/input.txt | tr ' ' '\n' | sort | uniq | awk '{print $0 " " NR+1}' >> ${dict}
+    wc -l ${dict}
 
     # make json labels
     data2json.sh --nj ${nj} --feat ${feat_sp_dir}/feats.scp --bpecode ${bpemodel}.model \
         data/${train_sp} ${dict} > ${feat_sp_dir}/data_${bpemode}${nbpe}.json
-    #data2json.sh --nj ${nj} --feat ${feat_dt_dir}/feats.scp --bpecode ${bpemodel}.model \
-    #    data/${train_dev} ${dict} > ${feat_dt_dir}/data_${bpemode}${nbpe}.json
+    data2json.sh --nj ${nj} --feat ${feat_dt_dir}/feats.scp --bpecode ${bpemodel}.model \
+        data/${train_dev} ${dict} > ${feat_dt_dir}/data_${bpemode}${nbpe}.json
 
-   # for rtask in ${recog_set}; do
-   #     feat_recog_dir=${dumpdir}/${rtask}/delta${do_delta}
-   #     data2json.sh --nj ${nj} --feat ${feat_recog_dir}/feats.scp --bpecode ${bpemodel}.model \
-   #         data/${rtask} ${dict} > ${feat_recog_dir}/data_${bpemode}${nbpe}.json
-   # done
+    for rtask in ${recog_set}; do
+        feat_recog_dir=${dumpdir}/${rtask}/delta${do_delta}
+        data2json.sh --nj ${nj} --feat ${feat_recog_dir}/feats.scp --bpecode ${bpemodel}.model \
+            data/${rtask} ${dict} > ${feat_recog_dir}/data_${bpemode}${nbpe}.json
+    done
 fi
 
 # You can skip this and remove --rnnlm option in the recognition (stage 5)
